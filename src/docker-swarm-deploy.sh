4e4e13ff0603f8c560ddc9bb37c37bf1f23a9e2b
Danyang Song <ds6118@nyu.edu>
2020-08-08 00:44:52 -0400

    Fix incorrect syntax for --secret.

--- src/docker-swarm-deploy.sh
+++ src/docker-swarm-deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Load the image
-docker load < ./spell-check-web-v1.0
+# # Load the image
+# docker load < ./spell-check-web-v1.0
 
-# # Or to build image accoding Dockerfile, use docker-compose.yml
-# docker-compose build
+# Or to build image accoding Dockerfile, use docker-compose.yml
+docker-compose build &&
 
 # Create a new docker swarm to deploy multiple services
-docker swarm leave --force && docker swarm init
+docker swarm leave --force && docker swarm init &&
 
 # Create new docker secrets
-echo admin | docker secret create admin_password -
+echo Administrator@1 | docker secret create admin_password -
 echo CS9163Assignment02WebsiteFlaskSessionSecretKey | docker secret create flask_session_secret_key -
 echo CS9163Assignment02WebsiteFlaskWTFCSRFToken | docker secret create flask_wtf_csrf_token -
 
@@ -27,4 +27,9 @@ echo CS9163Assignment02WebsiteFlaskWTFCSRFToken | docker secret create flask_wtf
 # src_web : the name of the image should be the same of the image that has been previously built.
 #         src_web is because this image was built according to docker-compose.yml,
 #         where the name is "web".
-docker service create --replicas 4 --limit-cpu 0.25 --limit-memory 100M --secret admin_password flask_session_secret_key flask_wtf_csrf_token --name spell-check-web -p 8080:5000 src_web:latest
+docker service create --replicas 4 \
+      --limit-cpu 0.25 --limit-memory 100M \
+      --secret admin_password \
+      --secret flask_session_secret_key \
+      --secret flask_wtf_csrf_token \
+      --name spell-check-web -p 8080:5000 src_web:latest
